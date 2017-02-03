c8d067c59cd18b56cee90fac8199b0f149b0b66b
Fenton Travers <fenton.travers@gmail.com>
2017-02-03 09:40:30 -0800

    init

---
+++ test/websocket_server/core_test.clj
@@ -0,0 +1,7 @@
+(ns websocket-server.core-test
+  (:require [clojure.test :refer :all]
+            [websocket-server.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
