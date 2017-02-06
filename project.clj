78dbdff39b5720c6b35eab3a468912355d778c45
Fenton Travers <fenton.travers@gmail.com>
2017-02-06 13:32:22 -0800

    works

--- project.clj
+++ project.clj
@@ -3,7 +3,9 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :dependencies [[org.clojure/clojure "1.9.0-alpha12"]
+                 [http-kit "2.2.0"]
+                 [com.taoensso/timbre "4.8.0"]]
   :main ^:skip-aot websocket-server.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/websocket_server/core.clj
+++ src/websocket_server/core.clj
@@ -1,7 +1,33 @@
 (ns websocket-server.core
-  (:gen-class))
+  (:require [org.httpkit.server :refer [on-close on-receive run-server
+                                        send! websocket? with-channel]]
+            [taoensso.timbre :refer [spy trace debug]]
+            [clojure.edn :refer [read-string]]))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+(defn callback [data]
+  (prn-str (spy :debug (read-string data))))
+
+(defonce server (atom nil))
+
+(defn websocket-handler [req]
+  (with-channel req channel
+    (on-close
+     channel
+     (fn [status] (debug (str "Websocket channel closed with status: " status))))
+    (on-receive
+     channel
+     (fn [data]
+       (if (websocket? channel)
+         (do
+           (trace (str "Recieved data: " data))
+           (send! channel (callback data))))))))
+
+(defn start
+  "Start websocket server"
+  ([] (start 7890))
+  ([port]
+   (debug (str "Starting websocket on port: [" port "]"))
+   (reset! server (run-server websocket-handler {:port port}))))
+
+(defn stop "Stop websocket server" [] (@server))
+(defn restart [] (stop) (start))
