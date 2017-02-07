36b47bc6fc331081617f9dbf82fad6be05d863ce
Fenton Travers <fenton.travers@gmail.com>
2017-02-06 21:17:53 -0800

    .

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject websocket-server "0.1.1"
+(defproject websocket-server "0.1.3-SNAPSHOT"
   :description "WebSocket Server Library"
   :url "https://github.com/ftravers/websocket-server"
   :license {:name "Eclipse Public License"
