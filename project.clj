7b7ac955bd517a7949e8f172e6da206f57001a3d
Fenton Travers <fenton.travers@gmail.com>
2017-02-15 11:33:05 -0800

    .

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject fentontravers/websocket-server "0.3.15"
+(defproject fentontravers/websocket-server "0.3.16-SNAPSHOT"
   :description "WebSocket Server Library"
   :url "https://github.com/ftravers/websocket-server"
   :license {:name "Eclipse Public License"
@@ -8,5 +8,10 @@
                  [com.taoensso/timbre "4.8.0"]
                  [org.clojure/core.async "0.2.395"]]
   :target-path "target/%s"
+
+  :repositories [["clojars" {:url "https://clojars.org/repo" :creds :gpg}]
+                 ["snapshots" {:url "https://clojars.org/repo" :creds :gpg}]
+                 ["releases" {:url "https://clojars.org/repo" :creds :gpg}]
+                 ["alternate" {:url "https://clojars.org/repo" :creds :gpg}]]
   
   :profiles {:uberjar {:aot :all}})
